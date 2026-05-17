1886af86d179db549c1635e9a6f73afd2e33237f
Syed Sameed Ahmed <sameed@example.com>
2026-05-17 19:11:14 +0530

    fix: resolve production db migrations, prisma client generation, and seed script execution for Render deployment

--- dist/prisma.config.js
+++ dist/prisma.config.js
@@ -8,7 +8,7 @@ exports.default = (0, config_1.defineConfig)({
         path: "prisma/migrations",
     },
     datasource: {
-        url: (0, config_1.env)("DATABASE_URL"),
+        url: process.env["DATABASE_URL"],
     },
 });
 //# sourceMappingURL=prisma.config.js.map
